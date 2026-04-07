2564024374192a13e25b838b0b04e4371bcb1be6
Sofiia <sofiia.asieieva@piano.io>
2026-04-07 22:53:03 +0300

    Implement robot movement logic towards specified coordinates

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,34 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() < toX) {
+            this.moveToGoal(robot, Direction.RIGHT, toX, true);
+        }
+        if (robot.getX() > toX) {
+            this.moveToGoal(robot, Direction.LEFT, toX, true);
+        }
+
+        if (robot.getY() < toY) {
+            this.moveToGoal(robot, Direction.UP, toY, false);
+        }
+        if (robot.getY() > toY) {
+            this.moveToGoal(robot, Direction.DOWN, toY, false);
+        }
+    }
+
+    private void moveToGoal(Robot robot, Direction direction,int targetValue, boolean isX) {
+        while (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
+        if (isX) {
+            while (robot.getX() != targetValue ) {
+                robot.stepForward();
+            }
+        } else {
+            while (robot.getY() != targetValue ) {
+                robot.stepForward();
+            }
+        }
+
     }
 }
